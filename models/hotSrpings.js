6ec11e86be6226e71ef2adb8fe0f0081921fc4e8
diamondallthewya <hunter@toplobster.io>
2023-03-30 13:59:13 -0500

    Created hotSprings.js schema

---
+++ models/hotSrpings.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const HotSpringsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true
+  },
+  address: {
+    type: String
+  },
+  country: {
+    type: String,
+    unique: true
+  },
+  state: {
+    type: String
+  },
+  city: {
+    type: String
+  },
+  lat: {
+    type: String,
+    unique: true
+  },
+  long: {
+    type: String,
+    unique: true
+  },
+  public: {
+    type: Boolean
+  },
+  hotel: {
+    type: Boolean
+  },
+  description: {
+    type: String
+  },
+  setting: {
+    type: String
+  },
+  temperature: {
+    type: String
+  },
+  createdAt: {
+    type: Date,default: () => Date.now()
+  },
+  updatedAt: {
+    type: Date, default: () => Date.now()
+  }
+})
+
+model.exports= mongoose.model('hotsprings', HotSpringsSchema);
