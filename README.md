bc49d429189a572563001d853840c00118ff81ac
Hunter Stevens <hunter@toplobster.io>
2023-04-08 15:57:46 -0500

    Updated Cors to allow for any URL, will update in the future

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # HSABackEnd
 
 ## Initial steps
-1) Create git project and back end repo
-2) Add README.md
-3) Best way to start up server in development is `npm run devStart`
+
+1. Create git project and back end repo
+2. Add README.md
+3. Best way to start up server in development is `npm run devStart`
--- server.js
+++ server.js
@@ -1,32 +1,37 @@
-require('dotenv').config()
-const express = require('express');
-const { mongo, default: mongoose } = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const { mongo, default: mongoose } = require("mongoose");
 const app = express();
-const HotSprings = require('./models/hotSprings');
+const cors = require("cors");
+const HotSprings = require("./models/hotSprings");
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/hotspringsdb');
-    console.log('Connected to MongoDB');
-  } catch(e) {
-    console.error('MongoDB connection failed');
+    await mongoose.connect(
+      process.env.MONGODB_URI || "mongodb://localhost:27017/hotspringsdb"
+    );
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.error("MongoDB connection failed");
   }
 }
 connect();
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/all', async (req, res) => {
+app.get("/all", async (req, res) => {
   const hotsprings = await HotSprings.find();
   res.json(hotsprings);
-})
+});
 
-app.get('/:country', (req, res) => {
-  res.json({sample: 'testing'})
-})
+app.get("/:country", (req, res) => {
+  res.json({ sample: "testing" });
+});
 
 const port = process.env.PORT || 3000;
 
